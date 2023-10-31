9664ecd952dbfb97afa08f65687a2f24677860bd
nassim <linnnux@hotmail.fr>
2023-10-31 16:03:00 +0100

    :v 0.0.3 update main.c

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 // fonction pour calculer le nombre de points
 int calculer_nb_points(int win, int draw)
 {
-
+    return 0;
 }
 
 // fonction pour calculer la différence de buts
 int calculer_diff_buts(int goalsFor, int goalsAgainst)
 {
-
+    return 0;
 }
 // Affiche un texte en utilisant uniquement les trois premières lettres
 void afficher_nom_trois_lettres(char str[])
@@ -23,22 +23,25 @@ void afficher_nom_trois_lettres(char str[])
 // Fonction pour charger les données à partir d'un fichier
 char load_data(const char *file_name, int *num_lines) 
 {
-
+    return 0;
 }
 
 // Fonction pour retourner le nombre de lignes d'un fichier
 int file_count(const char *file_name) 
 {
     FILE *file = fopen(file_name, "r");
+
+    return 0;
 }
 
 // Fonction pour retourner le nombre de champs
 int field_count(const char *line, const char *sep) {
 
+    return 0;
 }
 
 // Fonction pour afficher la matrice
-void display_matrix(int tab[][] , int num_lines, int num_fields) 
+void display_matrix(int tab[], int num_lines, int num_fields) 
 {
 
 }
@@ -56,9 +59,13 @@ int main()
  	// calculer le nombre de points
   	nbPts = calculer_nb_points( win, draw);
   	// afficher les points 
-  	print("%d pts\n", nbPts);
+  	printf("%d pts\n", nbPts);
 	
 	// calculer le différence de buts
+
+
+
+
 
 
     return 0;
